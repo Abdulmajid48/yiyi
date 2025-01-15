4ad9a6249a7869ca6a54fbde449b7348e901bcf3
Abdul-Majid Aladejana <aladejanamajid48@gmail.com>
2025-01-15 23:37:59 +0100

    initial commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,23 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { Image, StyleSheet, Platform, View, Text, Button} from 'react-native';
+
+
+
+
+export default function HomeScreen() {
+  return (
+  <View>
+    <Text>wvsfbmvnm,df</Text>
+    <Button title='ghdfnsfknfvkl' color={"red"}/>
+  </View>
+  );
+}
+
+
